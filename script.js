74728437127427543ad4bf326031f2a7e5151ebf
Caroline <113471098+carolinefreitasalegre@users.noreply.github.com>
2022-12-02 21:30:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,17 @@ function calcular() {
     const element = document.getElementById('text-area')
 
 
+    
      if (imc < 18.5) {
          element.textContent += `Olá ${nome}, seu IMC é de ${imc.toFixed(2)}, e você está magro. Bora comer!!!`
      } else if (imc < 24.9) {
          element.textContent += `Olá ${nome}, seu IMC é de ${imc.toFixed(2)}, e  você está com IMC normal.  Parabéns!!!`
      } else if (imc < 29.9) {
-         element.textContent += `Olá ${nome}, seu IMC é de ${imc.toFixed(2)}, e  você está com sobrepeso.  Cuide-se!!!`
-     } else if (imc < 23.9) {
-         element.textContent +=  `Olá ${nome}, seu IMC é de ${imc.toFixed(2)}, e  você está com obesida.  Cuide-se!!!`
-     } else if (imc < 24.9) {
-         element.textContent += `Olá ${nome}, seu IMC é de ${imc.toFixed(2)}, e  você está com obesidade mórbida.  Cuide-se!!!`
+         element.textContent += `Olá ${nome}, seu IMC é de ${imc.toFixed(2)}, e  você está com sobrepeso.  Sinal de alerta!!!`
+     } else if (imc < 34.9) {
+         element.textContent +=  `Olá ${nome}, seu IMC é de ${imc.toFixed(2)}, e  você está com obesidade. Alerta vermelha!!!`
+     } else if (imc < 39.9) {
+         element.textContent += `Olá ${nome}, seu IMC é de ${imc.toFixed(2)}, e  você está com obesidade mórbida.  Procure já o Dr. Nowzaradan !!!`
      }
     
 
